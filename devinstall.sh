1ec777ace3a58c783bfcf2a6ea6ed5644b7d1177
René Schultz Madsen <rm@microting.com>
2020-07-20 13:23:01 +0000

    Fixing the scripts to have correct paths.

--- devinstall.sh
+++ devinstall.sh
@@ -9,13 +9,13 @@ rm -fR Documents/workspace/microting/eform-angular-frontend/eform-client/e2e/Ass
 rm -fR Documents/workspace/microting/eform-angular-frontend/eform-client/e2e/Page\ objects/MyMicroting
 rm -fR Documents/workspace/microting/eform-angular-frontend/eform-client/wdio-plugin-step2.conf.js
 
-cp -av Documents/workspace/microting/eform-angular-my-microting-plugin/eform-client/src/app/plugins/modules/my-microting-pn Documents/workspace/microting/eform-angular-frontend/eform-client/src/app/plugins/modules/my-microting-pn
-cp -av Documents/workspace/microting/eform-angular-my-microting-plugin/eform-client/e2e/Tests/customer-settings Documents/workspace/microting/eform-angular-frontend/eform-client/e2e/Tests/customer-settings
-cp -av Documents/workspace/microting/eform-angular-my-microting-plugin/eform-client/e2e/Tests/customer-general Documents/workspace/microting/eform-angular-frontend/eform-client/e2e/Tests/customer-general
-cp -av Documents/workspace/microting/eform-angular-my-microting-plugin/eform-client/e2e/Assets/ Documents/workspace/microting/eform-angular-frontend/eform-client/e2e/Assets
-cp -av Documents/workspace/microting/eform-angular-my-microting-plugin/eform-client/e2e/Page\ objects/MyMicroting Documents/workspace/microting/eform-angular-frontend/eform-client/e2e/Page\ objects/MyMicroting
-cp -av Documents/workspace/microting/eform-angular-my-microting-plugin/eform-client/wdio-headless-plugin-step2.conf.js Documents/workspace/microting/eform-angular-frontend/eform-client/wdio-plugin-step2.conf.js
+cp -av Documents/workspace/microting/angular-my-microting-plugin/eform-client/src/app/plugins/modules/my-microting-pn Documents/workspace/microting/eform-angular-frontend/eform-client/src/app/plugins/modules/my-microting-pn
+cp -av Documents/workspace/microting/angular-my-microting-plugin/eform-client/e2e/Tests/customer-settings Documents/workspace/microting/eform-angular-frontend/eform-client/e2e/Tests/customer-settings
+cp -av Documents/workspace/microting/angular-my-microting-plugin/eform-client/e2e/Tests/customer-general Documents/workspace/microting/eform-angular-frontend/eform-client/e2e/Tests/customer-general
+cp -av Documents/workspace/microting/angular-my-microting-plugin/eform-client/e2e/Assets/ Documents/workspace/microting/eform-angular-frontend/eform-client/e2e/Assets
+cp -av Documents/workspace/microting/angular-my-microting-plugin/eform-client/e2e/Page\ objects/MyMicroting Documents/workspace/microting/eform-angular-frontend/eform-client/e2e/Page\ objects/MyMicroting
+cp -av Documents/workspace/microting/angular-my-microting-plugin/eform-client/wdio-headless-plugin-step2.conf.js Documents/workspace/microting/eform-angular-frontend/eform-client/wdio-plugin-step2.conf.js
 
 rm -fR Documents/workspace/microting/eform-angular-frontend/eFormAPI/Plugins/MyMicroting.Pn
 
-cp -av Documents/workspace/microting/eform-angular-my-microting-plugin/eFormAPI/Plugins/MyMicroting.Pn Documents/workspace/microting/eform-angular-frontend/eFormAPI/Plugins/MyMicroting.Pn
+cp -av Documents/workspace/microting/angular-my-microting-plugin/eFormAPI/Plugins/MyMicroting.Pn Documents/workspace/microting/eform-angular-frontend/eFormAPI/Plugins/MyMicroting.Pn
