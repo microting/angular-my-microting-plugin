0a3f531d700b7c59c17833da6ff8452ca56959dc
René Schultz Madsen <rm@microting.com>
2020-07-20 13:16:36 +0000

    Fixing the travis file and testinsinstall.sh

--- testinginstallpn.sh
+++ testinginstallpn.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 perl -pi -e '$_.="  },\n" if /INSERT ROUTES HERE/' src/app/plugins/plugins.routing.ts
-perl -pi -e '$_.="      .then(m => m.CustomersPnModule)\n" if /INSERT ROUTES HERE/' src/app/plugins/plugins.routing.ts
-perl -pi -e '$_.="    loadChildren: () => import('\''./modules/customers-pn/customers-pn.module'\'')\n" if /INSERT ROUTES HERE/' src/app/plugins/plugins.routing.ts
-perl -pi -e '$_.="    path: '\''customers-pn'\'',\n" if /INSERT ROUTES HERE/' src/app/plugins/plugins.routing.ts
+perl -pi -e '$_.="      .then(m => m.MyMicrotingPnModule)\n" if /INSERT ROUTES HERE/' src/app/plugins/plugins.routing.ts
+perl -pi -e '$_.="    loadChildren: () => import('\''./modules/my-microting-pn/my-microting-pn.module'\'')\n" if /INSERT ROUTES HERE/' src/app/plugins/plugins.routing.ts
+perl -pi -e '$_.="    path: '\''my-microting-pn'\'',\n" if /INSERT ROUTES HERE/' src/app/plugins/plugins.routing.ts
 perl -pi -e '$_.="  {\n" if /INSERT ROUTES HERE/' src/app/plugins/plugins.routing.ts
